f6161e544a77fba9ff9fb06fa33e8f6f6b1620c7
Yang Liu <yang.liu@shopify.com>
2024-09-17 16:59:44 -0400

    add temp debug code for ranker

--- src/generative_recommenders_pl/models/ranking.py
+++ src/generative_recommenders_pl/models/ranking.py
@@ -46,6 +46,75 @@ class Ranking(GenerativeRecommenders):
         )[0]  # [N', R]
         return logits
 
+    def ranking(self, batch: tuple[torch.Tensor]) -> torch.Tensor:
+        """
+        Retrieve the top-k items for the given sequence features.
+        """
+        # convert the batch to the sequence features (TODO: move to datamodule)
+        seq_features, target_ids, target_ratings = seq_features_from_row(
+            batch,
+            device=self.device,
+            max_output_length=self.gr_output_length + 1,
+        )
+
+        # embeddings
+        input_embeddings = self.embeddings.get_item_embeddings(seq_features.past_ids)
+
+        # input features preprocessor
+        past_lengths, default_user_embeddings, valid_mask, aux_mask = self.preprocessor(
+            past_lengths=seq_features.past_lengths,
+            past_ids=seq_features.past_ids,
+            past_embeddings=input_embeddings,
+            past_payloads=seq_features.past_payloads,
+        )
+
+        # forloop the top-k ids and get the user_embeddings
+        top_k_ids = seq_features.past_payloads["top_k_ids"]  # B, K
+        top_k_item_embeddings = self.embeddings.get_item_embeddings(
+            top_k_ids
+        )  # B, K, D
+        cached_states = None
+        for i in range(top_k_ids.size(1)):
+            processed_item_embeddings = self.preprocessor.get_processed_item_embeddings(
+                top_k_item_embeddings[:, i : i + 1, :]
+            )
+
+            # merge processed_item_embeddings with user_embeddings
+            # user_embeddings[:, past_lengths:past_lengths+1, :] = processed_item_embeddings
+            user_embeddings = (
+                default_user_embeddings + processed_item_embeddings
+            )  # this is wrong now
+
+            past_lengths += 1
+
+            valid_mask[:, past_lengths : past_lengths + 1] = 1
+
+            # sequence encoder
+            user_embeddings, cached_states = self.sequence_encoder(
+                past_lengths=past_lengths,
+                user_embeddings=user_embeddings,
+                valid_mask=valid_mask,
+                past_payloads=seq_features.past_payloads,
+                cached_states=cached_states,
+                return_cache_states=True,
+            )
+
+            current_embeddings = ops.get_current_embeddings(
+                seq_features.past_lengths, user_embeddings
+            )
+
+            logits = self.similarity(
+                input_embeddings=self.negatives_sampler.normalize_embeddings(
+                    current_embeddings
+                ),  # [N', D]
+                item_embeddings=self.negatives_sampler.normalize_embeddings(
+                    self.get_ratings_embeddings()
+                ).unsqueeze(0),  # [1, R, D]
+                item_sideinfo=None,
+                item_ids=None,
+            )[0]
+        return logits
+
     def training_step(self, batch: tuple[torch.Tensor], batch_idx: int) -> torch.Tensor:
         """Lightning calls this inside the training loop.
 
@@ -138,7 +207,8 @@ class Ranking(GenerativeRecommenders):
         seq_features = seq_features._replace(past_embeddings=input_embeddings)
 
         # forward pass
-        logits = self.logits(seq_features)
+        # logits = self.logits(seq_features)
+        logits = self.ranking(batch)
         self.metrics.update(preds=logits, target=target_ratings.squeeze(-1))
 
     def on_validation_epoch_end(self) -> None:
@@ -180,3 +250,13 @@ class Ranking(GenerativeRecommenders):
         self.metrics.reset()
         if "monitor" in self.configure_optimizer_params:
             return results[self.configure_optimizer_params["monitor"].split("/")[1]]
+
+    def predict_step(self, batch: tuple[torch.Tensor], batch_idx: int) -> torch.Tensor:
+        """Lightning calls this inside the predict loop.
+
+        Args:
+            batch (tuple[torch.Tensor]): A tuple containing the input and target
+                tensors.
+            batch_idx (int): The index of the batch.
+        """
+        return self.ranking(batch)
